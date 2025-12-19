d54304eee402b8c71c1585d16868792ad3d62ca2
proy <proyek1perpustakaa@gmail.com>
2025-12-19 11:55:25 +0700

    add profile_page + merapihkan alur reservasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:digital_libraria/views/pages/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
